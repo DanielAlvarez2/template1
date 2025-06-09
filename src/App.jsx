279898003bfb0c8da1a3a88b04a657a59c0f4cb9
Daniel Yllanes <daniel.yllanes@hotmail.com>
2025-06-09 15:43:17 -0400

    connect api to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,96 @@
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
+import {FaPlusCircle} from 'react-icons/fa'
+import {VscSave} from 'react-icons/vsc'
 
-export default function App() {
-  const [items, setItems] = useState([])
-  useEffect(()=>{
-    fetch('/api/items')
+export default function App(){
+  const [editForm, setEditForm] = useState(false)
+  const [dinnerItems, setDinnerItems] = useState([])
+  const [hiddenID, setHiddenID] = useState('')
+  const [submitText, setSubmitText] = useState(<FaPlusCircle />)
+  const getDinnerItems = ()=>{
+    fetch('/api/dinner')
       .then(res=>res.json())
-      .then(data=>setItems(data))
-  },[])
-  function renderItems(){
-    return items.map((item,i)=>{
-      return <div key={i}>
-        <h3>{item.name}</h3>
-        <p>Price: {item.price}</p>
-      </div>
-    })
+      .then(json=>setDinnerItems(json))
+      .catch(err=>console.log(err))
   }
-  return (
+  const deleteDinnerItem = async(id)=>{
+    await fetch(`/api/dinner/${id}`, {method:'DELETE'})
+      .then(console.log(`Deleted from Database`))
+      .then(async()=>await getDinnerItems())
+      .catch(err=>console.log(err))
+  }
+  useEffect(()=>getDinnerItems(),[])
+  async function addDinnerItem(formData){
+    await fetch('/api/dinner',{ method:'POST',
+                                headers:{'Content-Type':'application/json'},
+                                body: JSON.stringify({
+                                  section: formData.get('section'),
+                                  name: formData.get('name'),
+                                  allergies: formData.get('allergies'),
+                                  description: formData.get('description'),
+                                  price: formData.get('price')
+                                })
+    })
+      .then(console.log('Added to Database'))
+      .then(async ()=> await getDinnerItems())
+      .catch(err=>console.log(err))
+  }
+  async function updateDinnerItem(formData){
+    await fetch(`/api/dinner/${formData.get('id')}`,{ method:'PUT',
+                                                      headers: {'Content-Type':'application/json'},
+                                                      body: JSON.stringify({
+                                                        section: formData.get('section'),
+                                                        name: formData.get('name'),
+                                                        allergies: formData.get('allergies'),
+                                                        description: formData.get('description'),
+                                                        price: formData.get('price')
+                                                      })
+    })
+      .then(console.log(`Updated: ${formData.get('name')}`))
+      .then(setEditForm(false))
+      .then(async()=>await getDinnerItems())
+      .catch(err=>console.log(err))
+  }
+
+  function updateForm(id,section,name,allergies,description,price){
+    setHiddenID(id)
+    document.querySelector('#section').value = section
+    document.querySelector('#name').value = name
+    document.querySelector('#allergies').value = allergies
+    document.querySelector('#description').value = description
+    document.querySelector('#price').value = price
+    setEditForm(true)
+  }
+
+  return(
     <>
-      <h1>EXAMPLE WEB SHOP</h1>
-      {renderItems()}
+      <form action={editForm?updateDinnerItem:addDinnerItem}>
+        <input type='hidden' id='id' name='id' value={hiddenID} />
+        <label>
+          Section:
+          <input id='section' name='section' placeholder='Section' type='text' />
+        </label>
+        <label>
+          Name:
+          <input id='name' name='name' placeholder='Name' type='text' />
+        </label>
+        <label>
+          Allergies:
+          <input id='allergies' name='allergies' placeholder='allergies' type='text' />
+        </label>
+        <label>
+          Description:
+          <input id='description' name='description' placeholder='Description' type='text' />
+        </label>
+        <label>
+          Price:
+          <input id='price' name='price' placeholder='Price' type='text' />
+        </label>
+        <button style={editForm?{background:'blue'}:{background:'black'}}>
+          {editForm?<><VscSave /> Save Changes</> : <><FaPlusCircle /> Add Item</>}
+        </button>
+
+      </form>
     </>
   )
 }
-
